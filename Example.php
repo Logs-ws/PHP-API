0bc690c8a97f7f7c781ee1175d79d03aa546bcb4
Shouvik Chatterjee <hello@shouvik.net>
2014-04-06 15:12:05 +0530

    Modified library as per new API v1.1

--- Example.php
+++ Example.php
@@ -1,12 +1,11 @@
 <?php
 require 'Logs.php';
 
-$obj = new Logs('your-api-key', 'json');
+$obj = new Logs('YOUR-API-KEY-HERE', 'json');
 
-//Log that you want to send to the logs.ws server
+// Log that you want to send to the logs.ws server
 $log ='Test log.'; 
 
-//Send the log to the logs.ws server using the sendRequest() method that accepts two arguments. 
-//allowed log types are "INFO", "WARNING", "ERROR"
-echo $obj->sendRequest($log, 'INFO'); 
+// Send the log to the logs.ws server/
+echo $obj->Send($log, 'INFO'); 
 ?>
